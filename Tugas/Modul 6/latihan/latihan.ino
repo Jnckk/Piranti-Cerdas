bac57133959d190fc79f8fab624629e35e62135b
Riski Cahyadi <96654537+Jnckk@users.noreply.github.com>
2023-12-11 17:15:44 +0700

    Add files via upload

--- Tugas/Modul 6/latihan/latihan.ino
+++ Tugas/Modul 6/latihan/latihan.ino
@@ -6,14 +6,13 @@
 #define BLYNK_TEMPLATE_ID "TMPL6mFGVAlSv"
 #define BLYNK_TEMPLATE_NAME "Latihan Praktikum Modul 5"
 #define BLYNK_AUTH_TOKEN "tZ9Z5YAL8WFHVlNMf5EbFC08klNptbjb"
-#define DHT_PIN 4       // Pin koneksi sensor DHT
-#define BUZZER_PIN 5    // Pin koneksi Buzzer
+#define DHT_PIN 4
+#define BUZZER_PIN 5
 
 BlynkTimer timer;
 
-// Konfigurasi nama hotspot/SSID beserta password
 char auth[] = BLYNK_AUTH_TOKEN;
-char ssid[] = "SSID WIFI";
+char ssid[] = "SSID";
 char pass[] = "PASSWORD";
 
 DHT dht(DHT_PIN, DHT11);
@@ -23,27 +22,26 @@ void send_data() {
   Serial.print("Temperature: ");
   Serial.println(temperature);
 
-  // Keterangan suhu
   String suhu_keterangan;
 
   if (temperature > 30.0) {
     suhu_keterangan = "Panas";
-    // Suhu panas, bunyikan Buzzer dengan frekuensi tinggi
+    Serial.println("Status: Panas");
     tone(BUZZER_PIN, 1000, 500); // 1000 Hz for 500 milliseconds
   } else if (temperature > 25.0) {
     suhu_keterangan = "Hangat";
-    // Suhu hangat, bunyikan Buzzer dengan frekuensi sedang
+    Serial.println("Status: Hangat");
     tone(BUZZER_PIN, 500, 200); // 500 Hz for 200 milliseconds
   } else {
     suhu_keterangan = "Normal";
-    // Suhu normal, matikan Buzzer
+    Serial.println("Status: Normal");
     noTone(BUZZER_PIN);
   }
 
-  // Mengirim suhu dan keterangan ke Blynk
   Blynk.virtualWrite(V0, temperature);
   Blynk.virtualWrite(V1, suhu_keterangan);
 }
+
 
 
 void setup() {
@@ -51,10 +49,8 @@ void setup() {
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
   dht.begin();
   
-  // Mengatur pin Buzzer sebagai OUTPUT
   pinMode(BUZZER_PIN, OUTPUT);
 
-  // Menjadwalkan fungsi send_data setiap 1000ms (1 detik)
   timer.setInterval(1000L, send_data);
 }
 
